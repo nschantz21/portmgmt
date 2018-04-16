e2112a5bd7a12c2ea1da353b6b49564b1e76336e
nschantz21 <nschantz21@gmail.com>
2018-04-16 15:52:23 -0400

    latest

---
+++ portfolio_tools.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from pandas import Series
+
+def normalize(port, total = 1.0):
+    return port / port.sum() * total
+    
+def clean(port, limit = 0.0, norm = True):
+    tmp = port[port > limit]
+    if norm:
+        return normalize(tmp)
+    return tmp
+ 
+def replace(port, to_replace, replacements=None, lvl=None):
+    if lvl is not None:
+        gport = port.groupby(level=lvl).sum()
+        rport = replace(gport, to_replace, replacements)
+        rport.index.rename(lvl, inplace=True)
+        mult = rport.divide(gport, level=lvl, fill_value=1.0)
+        merged = pd.merge(port.rename().reset_index(), mult.reset_index(), on=lvl, how='left')
+        result = merged['0_x'] * merged['0_y']
+        result.index = port.index
+        return result
+    if replacements is None:
+        new_index = port.index ^ to_replace.index
+        replacements = Series((1.0 / len(new_index)), copy=True, index = new_index)
+    nominal_percent = (port * to_replace).dropna()
+    new_series = port.subtract(nominal_percent, fill_value = 0.0)
+    new_series = new_series.add((replacements * nominal_percent.sum()), fill_value = 0.0)
+    return new_series
+
+
+def restrict(port, limit, to_replace=None, rebal=True, replacements=None, lvl=None, normed=True):
+    if lvl is not None:
+        gport = port.groupby(level=lvl).sum()
+        rport = restrict(gport, limit, to_replace, rebal, replacements)
+        rport.index.rename(lvl, inplace=True)
+        mult = rport.divide(gport, level=lvl, fill_value=1.0)
+        merged = pd.merge(port.rename().reset_index(), mult.reset_index(), on=lvl, how='left')
+        result = merged['0_x'] * merged['0_y']
+        result.index = port.index
+        return result
+    if to_replace is None:
+        to_replace = port.index
+        # maybe add default behavior for case when replacements is supplied and to_replace is not
+        if replacments is not None:
+        raise NotImplementedError('You cannot supply replacements without supplying to_replace argument')
+        if limit < (1.0 / len(to_replace)):
+            raise ValueError('{lim} is too small of an upper limit for {lng} positions. {u_lim} is the smallest upper limit you can have.'.format(lim=limit, lng = len(to_replace), u_lim = 1.0/len(to_replace)))
+    over = port[to_replace].loc[port > limit]
+    excess = (over - limit) / over
+    restrict_series = replace(port, excess, replacements)
+    if normed:
+        restrict_series = restrict_series / restrict_series.sum()
+    if rebal:
+        while (restrict_series[to_replace] > limit).any():
+            over = restrict_series[to_replace].loc[restrict_series > limit]
+            xs = (over - limit) / over
+            restrict_series = replace(restrict_series, to_replace = xs, replacements = replacements)
+    return restrict_series
+
+
+# maybe go recursively down through levels for MultiIndex series
--- test_script.py
+++ test_script.py
@@ -1,24 +1,24 @@
 import pandas as pd
 from pandas import Series
 
-
 # test data
 test_port = pd.read_csv('test/test_portfolio.csv', index_col = ['name', 'country', 'industry'])['weight'].rename()
 
 lvl = ['country', 'industry']
 indx = pd.MultiIndex.from_tuples([('GB', 'O')], names=lvl)
-rport = pd.Series(data=[.9], index=indx)
+rport1 = pd.Series(data=[.9], index=indx)
+tp1 = replace(test_port, rport1, lvl=lvl)
+
 # replace works for MultiIndex, which I think isn't bad
-rport = pd.Series({'O':1.0})
-tp2 = replace(test_port, rport, lvl='industry')
+rport2 = pd.Series({'O':1.0})
+tp2 = replace(test_port, rport2, lvl='industry')
+
+rplcmnts = pd.Series({'N':.5})
+tp3 = replace(test_port, rport2, replacements=rplcmnts, lvl='industry')
 
 ############
 # Restrict #
 ############
-port = test_port
-limit = .4
-to_replace = None
-rebal = True
 
 #replacements = pd.Series(data=[1.0], index=pd.MultiIndex.from_tuples([('A', 'US', 'M')], names = ['name', 'country', 'industry']))
 replacements=pd.Series({'M':1.0})
@@ -26,3 +26,18 @@ lvl = 'industry'
 
 # to individually Restrict a position
 restrict(port, limit, to_replace, rebal=False, replacements=replacements, lvl=lvl)
+
+name_port = port.groupby('name').sum()
+rn = restrict(name_port, .1)
+rn2 = restrict(port, .1, lvl='name')
+
+rc = restrict(port, limit=.3, lvl='country')
+rc.groupby('country').sum()
+
+ri = restrict(port, limit=.15, to_replace = ['O', 'M'], replacements = pd.Series({'N':1.0}), lvl='industry')
+ri.groupby('industry').sum()
+
+lvl = ['country', 'industry']
+indx = pd.MultiIndex.from_tuples([('GB', 'O')], names=lvl)
+rci = restrict(test_port, .1, to_replace = indx, lvl=lvl)
+
